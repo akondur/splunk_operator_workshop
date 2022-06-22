31a00e3d8c582808a7e128e49bb9e6847bd4a22f
Arjun Kondur <69369319+akondur@users.noreply.github.com>
2022-06-22 08:29:04 -0700

    Update shcwithidxcingressservice.sh

--- config/shcwithidxcingressservice.sh
+++ config/shcwithidxcingressservice.sh
@@ -70,34 +70,12 @@ status:
     - ip: ${IP_ADDR}
 EOF
 
-cat <<EOF | $kubectl apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: nova-mc
-spec:
-  selector:
-    app.kubernetes.io/instance: splunk-default-monitoring-console
-  ports:
-    - protocol: TCP
-      port: 8002
-      targetPort: 8000
-      name: cm
-  type: LoadBalancer
-status:
-  loadBalancer:
-    ingress:
-    - ip: ${IP_ADDR}
-EOF
-
 echo "==========endpoints==========="
 
 echo "Search Head UI at: http://${IP_ADDR}:8000"
 echo "HEC endpoint at: https://${IP_ADDR}:8088"
 echo "S2S at: ${IP_ADDR}:9997"
 echo "Cluster Master at: http://${IP_ADDR}:8001"
-echo "Monitoring Console at: http://${IP_ADDR}:8002"
-
 
 echo ""
 echo "Happy Splunking!!!"
